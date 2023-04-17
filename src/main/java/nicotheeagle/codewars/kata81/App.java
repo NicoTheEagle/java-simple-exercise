362b06e56d9a6f3b99c0f2e0e4455278899e5531
NicoTheEagle <130918767+NicoTheEagle@users.noreply.github.com>
2023-04-17 13:28:20 +0200

    Esercizio di conversione
    Create a function which translates a given DNA string into RNA.
    
    For example:
    
    "GCAT"  =>  "GCAU"
    The input string can be of arbitrary length - in particular, it may be empty. All input is guaranteed to be valid, i.e. each input string will only ever consist of 'G', 'C', 'A' and/or 'T'.

---
+++ src/main/java/nicotheeagle/codewars/kata81/App.java
@@ -0,0 +1,17 @@
+package nicotheeagle.codewars.kata81;
+
+import java.util.Scanner;
+
+public class App {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Bio b = new Bio();
+		String dna;
+		
+		System.out.println("Inserire 4 caratteri tra 'G' 'C' 'A' 'T':");
+		dna = sc.nextLine();
+		System.out.println("La tua nuova stringa è: " + b.dnaToRna(dna));
+	}
+
+}
---
+++ src/main/java/nicotheeagle/codewars/kata81/Bio.java
@@ -0,0 +1,8 @@
+package nicotheeagle.codewars.kata81;
+
+public class Bio {
+	
+    public String dnaToRna(String dna){
+        return dna.replace("T", "U");
+    } 
+}
