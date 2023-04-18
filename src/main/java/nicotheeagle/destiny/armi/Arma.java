fd30db9fa779a40ee732c14122d8a851f0b5e411
NicoTheEagle <130918767+NicoTheEagle@users.noreply.github.com>
2023-04-19 01:50:53 +0200

    Inizializzata funzionalità armi

--- src/main/java/nicotheeagle/destiny/armi/Arma.java
+++ src/main/java/nicotheeagle/destiny/armi/Arma.java
@@ -2,48 +2,41 @@ package nicotheeagle.destiny.armi;
 
 public class Arma {
 	
-	private String name;
-	private GunType tipoArma;
-	private AmmoType tipoMuni;
-	private int impatto;
-	private int gittata;
-	private int stabilita;
+	private final String name;
+	private final GunType tipoArma;
+	private final AmmoType tipoMuni;
+	private final int impatto;
+	private final int gittata;
+	private final int stabilita;
+	
+	public Arma(String name, GunType tipoArma, AmmoType tipoMuni, int gittata, int impatto, int stabilita) {
+		
+		this.name = name;
+		this.tipoArma = tipoArma;
+		this.tipoMuni = tipoMuni;
+		this.impatto = impatto;
+		this.gittata = gittata;
+		this.stabilita = stabilita;
+
+	}
 	
 	public String getName() {
 		return name;
 	}
-	public void setName(String name) {
-		this.name = name;
-	}
 	public GunType getTipoArma() {
 		return tipoArma;
-	}
-	public void setTipoArma(GunType tipoArma) {
-		this.tipoArma = tipoArma;
 	}
 	public AmmoType getTipoMuni() {
 		return tipoMuni;
 	}
-	public void setTipoMuni(AmmoType tipoMuni) {
-		this.tipoMuni = tipoMuni;
-	}
 	public int getImpatto() {
 		return impatto;
-	}
-	public void setImpatto(int impatto) {
-		this.impatto = impatto;
 	}
 	public int getGittata() {
 		return gittata;
 	}
-	public void setGittata(int gittata) {
-		this.gittata = gittata;
-	}
 	public int getStabilita() {
 		return stabilita;
-	}
-	public void setStabilita(int stabilita) {
-		this.stabilita = stabilita;
 	}
 	
 	
---
+++ src/main/java/nicotheeagle/destiny/armi/WeaponFactory.java
@@ -0,0 +1,16 @@
+package nicotheeagle.destiny.armi;
+
+public class WeaponFactory {
+	
+	
+	public static Arma iniziaDesolatore() {
+		return new Arma("Desolatore M5", GunType.FUCILE_A_POMPA, AmmoType.SPECIALI, 65, 44, 40);
+	}
+	
+	public static Arma drop() {
+		
+		
+		return null;
+	}
+	
+}
