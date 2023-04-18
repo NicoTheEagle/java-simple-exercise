97b326b8ec66bdbd456d889ab7d042c49bc374f9
NicoTheEagle <130918767+NicoTheEagle@users.noreply.github.com>
2023-04-19 00:09:54 +0200

    Creato test minimo

---
+++ src/test/java/nicotheeagle/codewars/kata83/AppTest.java
@@ -0,0 +1,17 @@
+package nicotheeagle.codewars.kata83;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+	@Test
+	public void minTest() {
+		int[] arr = new int[] {-52, 56, 30, 29, -54, 0, -110};
+		int expected = -110;
+		
+		int min = App.min(arr);
+		
+		assertEquals(expected, min);
+	}
+}
