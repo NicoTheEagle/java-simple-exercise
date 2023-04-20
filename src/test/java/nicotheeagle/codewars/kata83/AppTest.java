003de428aae0fd2accbbd3f1ea494b6574c7edd2
Lorenzo Gandino <lorenzoga94@gmail.com>
2023-04-20 18:23:37 +0200

    fix kata83.AppTest

--- src/test/java/nicotheeagle/codewars/kata83/AppTest.java
+++ src/test/java/nicotheeagle/codewars/kata83/AppTest.java
@@ -2,25 +2,28 @@ package nicotheeagle.codewars.kata83;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import org.junit.jupiter.api.Test;
 
 public class AppTest {
 	@Test
 	public void minTest() {
-		int[] arr = new int[] {-52, 56, 30, 29, -54, 0, -110};
+		Integer[] arr = new Integer[] {-52, 56, 30, 29, -54, 0, -110};
 		int expected = -110;
 		
-		int min = App.min(arr);
+		int min = App.min(new ArrayList<>(Arrays.asList(arr)));
 		
 		assertEquals(expected, min);
 	}
 
 	@Test
 	public void maxTest() {
-		int[] arr = new int[] {-52, 56, 30, 29, -54, 0, -110};
+		Integer[] arr = new Integer[] {-52, 56, 30, 29, -54, 0, -110};
 		int expected = 56;
 		
-		int max = App.max(arr);
+		int max = App.max(new ArrayList<>(Arrays.asList(arr)));
 		
 		assertEquals(expected, max);
 	}
